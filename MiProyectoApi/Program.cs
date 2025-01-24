b6834a1fbedb2d938795abdc1a5a5b0ea86b32d0
Maria <julianahenao1510@gmail.com>
2025-01-24 13:30:54 -0500

    Subo MiProyectoApi con el codigo desacoplado, y con put, delete y el if de get

---
+++ MiProyectoApi/Program.cs
@@ -0,0 +1,18 @@
+using ProyectoApi.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+WeatherForecastController.ConfiguracionRutas(app);
+
+app.Run();

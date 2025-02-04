077e1bb51803b482bb5c9c46b63138e312409187
Maria <julianahenao1510@gmail.com>
2025-02-04 16:39:48 -0500

    Intento de que estos archivos dejen de generar cambios automaticos

--- Calculadora/Calculadora/obj/Debug/net9.0/Calculadora.AssemblyInfo.cs
+++ Calculadora/Calculadora/obj/Debug/net9.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+157c851a72e427af93df87adbd81b7f429f82598")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a5cdcc185f15593aca1f2e0b3921563910125c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

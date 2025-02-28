fe4980fe5be2e0f249492117e0bb32c536352917
Maria <julianahenao1510@gmail.com>
2025-02-28 08:04:08 -0500

    Eliminando carpetas bin y obj del seguimiento

--- AppProducts/Dtos/CategoryDto.cs
+++
@@ -1,8 +0,0 @@
-namespace AppProducts.Dtos
-{
-    public class CategoryDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
--- AppProducts/Models/Category.cs
+++
@@ -1,8 +0,0 @@
-namespace AppProducts.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
